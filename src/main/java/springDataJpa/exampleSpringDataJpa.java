ecaf09332e4c02480e8c7795679aaccabb8d5998
vinokurovaa <aa.vinokurov@mail.ru>
2018-08-21 09:41:12 +0300

    add spring data jpa audit

--- src/main/java/springDataJpa/exampleSpringDataJpa.java
+++ src/main/java/springDataJpa/exampleSpringDataJpa.java
@@ -3,6 +3,7 @@ package springDataJpa;
 
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+import java.util.Date;
 import java.util.List;
 
 public class exampleSpringDataJpa {
@@ -11,18 +12,43 @@ public class exampleSpringDataJpa {
         ctx.load("META-INF/Spring/spring-data-jpa.xml");
         ctx.refresh();
 
-        ContactService contactService = ctx.getBean("ContactService", ContactService.class);
+        /*ContactService contactService = ctx.getBean("ContactService", ContactService.class);
 
         listContacts("find All = ", contactService.findAll());
-        listContacts("find by fist Name = ", contactService.findByFirstName("Chris"));
+        listContacts("find by fist Name = ", contactService.findByFirstName("Chris"));*/
+
+        ContactAuditService contactService = ctx.getBean(
+                "contactAuditService", ContactAuditService.class);
+
+        List<ContactAudit> contacts = contactService.findAll();
+        listContacts(contacts);
+
+        System.out.println("Add new contact");
+        ContactAudit contact = new ContactAudit();
+        contact.setFirstName("Michael");
+        contact.setLastName("Jackson");
+        contact.setBirthDate(new Date());
+        contactService.save(contact);
+        contacts = contactService.findAll();
+        listContacts(contacts);
+
+        contact = contactService.findById(1l);
+        System.out.println("");
+        System.out.println("Contact with id 1:" + contact);
+        System.out.println("");
+
+        System.out.println("Update contact");
+        contact.setFirstName("Tom");
+        contactService.save(contact);
+        contacts = contactService.findAll();
+        listContacts(contacts);
 
 
     }
 
-    private static void listContacts(String message, List<Contact> contacts) {
+    private static void listContacts(List<ContactAudit> contacts) {
         System.out.println("");
-        System.out.println(message);
-        for (Contact contact: contacts) {
+        for (ContactAudit contact: contacts) {
             System.out.println(contact);
             System.out.println();
         }
